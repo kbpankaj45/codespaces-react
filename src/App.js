052e530e64fad1c83140c3f3db66669a2aa6ebc3
kbpankaj45 <147134178+kbpankaj45@users.noreply.github.com>
2023-10-07 06:53:13 +0000

    my first commit to code space

--- src/App.js
+++ src/App.js
@@ -8,19 +8,7 @@ function App() {
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <h1>Welcome pankaj to codespace</h1>
       </header>
     </div>
   );
